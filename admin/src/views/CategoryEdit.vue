53900b671c2a6c744d982ad6b3cf1a44fd923f31
qiakai <2368431928@qq.com>
2021-02-27 18:24:14 +0800

    第二次提交

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,12 @@
   <div class="about">
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option v-for="item in parents" :key="item._id" 
+          :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -20,18 +26,36 @@ export default {
   data() {
     return {
       model: {},
+      parents: [],
     };
   },
   methods: {
     async save() {
       // console.log('save')
-      const res = await this.$http.post("categories", this.model); // eslint-disable-line no-unused-vars
+      let res; // eslint-disable-line no-unused-vars
+      if (this.id) {
+        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("rest/categories", this.model); 
+      }
       this.$router.push("/categories/list");
       this.$message({
         type: "success",
         message: "保存成功",
       });
     },
+    async fetch() {
+      const res = await this.$http.get(`rest/categories/${this.id}`);
+      this.model = res.data;
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`rest/categories`);
+      this.parents = res.data;
+    },
+  },
+  created() {
+    this.fetchParents()
+    this.id && this.fetch();
   },
 };
 </script>
--- server/models/Category.js
+++ server/models/Category.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name: { type: String } 
+    name: { type: String },
     // name: String
+    parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' },
 })
 
 //实例这个category并导出
-module.exports = mongoose.model('Category',schema)
+module.exports = mongoose.model('Category', schema)
