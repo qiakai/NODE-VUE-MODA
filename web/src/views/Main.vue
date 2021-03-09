dd33666d5941a10be473b06374c3a9d0ce8618b9
qiakai <2368431928@qq.com>
2021-03-10 00:20:43 +0800

    第十一次提交

---
+++ web/src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="topbar">
+      topbar
+    </div>
+    <div class="nav">
+      nav
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
