f0e13ce83c00f9482d6b1f4faae687fa4cdd765b
qiakai <2368431928@qq.com>
2021-03-10 23:48:31 +0800

    第十二次提交

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,11 +1,77 @@
 <template>
   <div>
-    首页
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/b9905b35bb0afa9050d9ddbe04d82d29.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="../assets/images/ddc8c8922cbb694dfb73c86bb03fce22.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right px-3"
+        slot="pagination"
+      ></div>
+    </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3 d-flex flex-wrap text-center text-grey-1 pt-3">
+      <div class="nav-item mb-3" v-for="n in 10" :key="n">
+        <i class="sprite sprite-news"></i>
+        <div>爆料站</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".pagination-home",
+        },
+        // Some Swiper option/callback...
+        autoplay: {
+            delay:2500,
+            disableOnInteraction: false,
+        },
+      },
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+@import "../assets/scss/variables";
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($map: $colors, $key: "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($map: $colors, $key: "info");
+    }
+  }
+}
+.nav-icons {
+  .nav-item {
+    width: 25%;
+  }
+}
+</style>
